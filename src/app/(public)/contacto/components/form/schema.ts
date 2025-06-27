377ba1fffbfc04266304f57dd0e6acb2cae7def1
Carlos Jesús Barrantes Saldarriaga <carlos2004jbs2307@gmail.com>
2025-06-27 08:55:41 -0500

    Fix ContactFormDefaultValues

--- src/app/(public)/contacto/components/form/schema.ts
+++ src/app/(public)/contacto/components/form/schema.ts
@@ -1,19 +1,19 @@
 import { z } from "zod";
 
-export const SERVICE_TYPES = ["Contabilidad", "Jurídico", "Digital"] as const;
+// export const SERVICE_TYPES = ["Contabilidad", "Jurídico", "Digital"] as const;
 
 export const contactSchema = z.object({
   fullName: z.string().min(2, { message: "El nombre es obligatorio" }),
   email: z.string().email({ message: "Ingrese un correo electrónico válido" }),
   phone: z.string().optional(),
-  typeService: z.preprocess(
+  /*typeService: z.preprocess(
     (val) => (val === "" ? undefined : val),
     z
       .enum(SERVICE_TYPES, {
         required_error: "Por favor seleccione un servicio",
       })
       .optional()
-  ),
+  ),*/
   message: z
     .string()
     .min(10, { message: "El mensaje debe tener al menos 10 caracteres" })
@@ -26,6 +26,6 @@ export type ContactFormDefaultValues = {
   fullName:  string;
   email: string;
   phone: string;
-  typeService: (typeof SERVICE_TYPES)[number] | undefined;
+  // typeService: (typeof SERVICE_TYPES)[number] | undefined;
   message: string;
 };
