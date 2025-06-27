502b9fefa9b7ad0300fc44cf02fce253db8d28cb
Carlos Jesús Barrantes Saldarriaga <carlos2004jbs2307@gmail.com>
2025-06-27 00:23:38 -0500

    Solution page props

--- src/app/(public)/proyectos/[id]/page.tsx
+++ src/app/(public)/proyectos/[id]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default async function ProjectPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
 
